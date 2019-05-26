0d0ff1f474cfcf7e5d2f2098e631d45a63fd38c6
Dragos Paun <dragos.paun0000@gmail.com>
2019-05-26 10:36:23 +0000

    test for connection to the DB

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
 import os
 from flask import Flask, flash, render_template, redirect, request, url_for, session
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId 
 
 app = Flask(__name__)
+app.secret_key = 'some_secret' 
+app.config["MONGO_DBNAME"] = 'cook_book'
+app.config["MONGO_URI"] ='mongodb://dude:h310db@ds149706.mlab.com:49706/cook_book'
+
+mongo = PyMongo(app)
 
 @app.route('/')
-def index():
-    return 'flask app test OK'
+@app.route('/get_recipes')
+def get_tasks():
+    return render_template("recipes.html", recipes=mongo.db.recipes.find())
     
     
     
