9b2bcb4c4ac986d017aaf9d1176213c0cd1e8bfa
Dragos Paun <dragos.paun0000@gmail.com>
2019-06-23 21:06:28 +0000

    added add recipe page

--- app.py
+++ app.py
@@ -17,6 +17,34 @@ def get_recipes():
     
     
     
+@app.route('/add_recipe')
+def add_recipe():
+    return render_template("addRecipe.html",
+    seasons=mongo.db.seasons.find(),
+    difficulties=mongo.db.difficulties.find(),
+    types=mongo.db.types.find())
+    
+    
+@app.route('/insert_recipe', methods=["POST"])
+def insert_recipe():
+    recipe=mongo.db.recipes
+    ingredients_needed=request.form.getlist('ingredients')
+    recipe_form_fields = {
+        'recipe_name':request.form['name_of_recipe'],
+        'author':request.form['author'],
+        'season':request.form['season'],
+        'serves': request.form['serves'],
+        'type': request.form['type'],
+        'difficulty':request.form['difficulty'],
+        'prep_time': request.form['prep_time'],
+        'rating':request.form['rating'],
+        'image_url': request.form['image_url'],
+        'ingredients': ingredients_needed,
+        'method': request.form['method']
+    }
+    recipe.insert_one(recipe_form_fields)
+    return redirect(url_for('get_recipes'))
+
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
         port=int(os.environ.get('PORT')),
---
+++ static/js/addIngredients.js
@@ -0,0 +1,24 @@
+
+function addIngredient() {
+
+    let maxOfIngredients = 10; 
+    let addIngredientButton = $('.add_button'); 
+    let addIngredientDiv = $('.ingredient_div');
+    let addIngredientFieldHtml = '<div class="row"><div class="input-field ingredient_div col s12"><i class="material-icons prefix">local_grocery_store</i><input type="text" name="ingredients" value="" required/><label for="icon_prefix">Add Ingredient</label></div><a href="javascript:void(0);" class="remove_button validate"><i class="material-icons prefix">clear</i></a></div>'; 
+    let ingredientNumber = 1;
+
+    //add ingredient
+    $(addIngredientButton).click(function() {
+        if (ingredientNumber < maxOfIngredients) {
+            ingredientNumber++; 
+            $(addIngredientDiv).append(addIngredientFieldHtml);
+        }
+    });
+
+    //remove ingredient 
+    $(addIngredientDiv).on('click', '.remove_button', function(e) {
+        e.preventDefault();
+        $(this).parent('div').remove();
+        ingredientNumber--; 
+    });
+};
